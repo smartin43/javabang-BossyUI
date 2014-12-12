7885c30ea84c83de391e54e2dc3f75d411af2c65
AlmondSeason <almondseason@gmail.com>
2014-12-12 02:07:58 -0800

    Added javaDoc tags and information
    Added javaDoc tags and information

--- src/directives/bossy.dropdown.js
+++ src/directives/bossy.dropdown.js
@@ -1,9 +1,43 @@
+/**
+* bossy.dropdown is a widget that allows the user to utilize an already functional dropdown
+* The user MUST create a "config" object - This object and its data members will be used to serialize all the data necessary for the dropdown creation
+*
+* Data members part of the config object are "src", "title", "selected", "template"
+* The user MUST pass the "src" - the location of the json data that will populate the list
+* The user MUST pass a "title" - the data member of the object that the user wants to populate the list with and order the dropdown with
+* The user WILL be able to access "selected" - the object currently selected in the dropdown; this will be an empty object; DO NOT SERIALIZE
+* The user CAN provide a "template" - to change the look of the dropdown, otherwise a default template will be utilized
+* @since        1.0
+* @author 		Alen Maragoul
+* @author 		Shelby Martin
+* @author 		Patrick Barnum
+*/
+
+/**
+* @see 		EXAMPLE FOR DROPDOWN CREATION
+*
+* @see 		angular.module('testModule', ['bossy.dropdown'])
+* @see 			.controller('testController', ['$scope', function($scope) {	
+* @see 			$scope.config = {
+* @see 				src: 'states.json',
+* @see 				title: 'name',
+* @see 				select: {},
+* @see 				template: 'testTemplate10.html'
+* @see 			};
+* @see 		}])
+*/
+
 angular.module('bossy.dropdown', [])
 	.run(function($templateCache){
         $templateCache.put('bossy-dropdown.html', '<div><select ng-options="item[dropdown.title] for item in dropdown.items | orderBy: dropdown.title" ng-model="selectedItem" ng-change="dropdown.updateSelectedItem(selectedItem)"><option value="" ng-hide="selectedItem">Please select one...</option></select></div>');	
     })
 	.directive('bossyDropdown', function($http, $compile) {
 		return {
+			/** 
+			 * @param config   config object passed by the user
+			 * @param select    returns object that is currently selected in dropdown
+			 * @param items     array of items that populate the dropdown
+			 */
 			restrict: 'EA',
 			scope: {
 				config: "=",
@@ -12,6 +46,9 @@ angular.module('bossy.dropdown', [])
 			},
 			templateUrl: '',
 			link: function(scope, element, attrs) {
+				/** 
+				*	@param customTemplate 	Template specified by user; else use default template
+				*/
 				var customTemplate;
 
 				//Checks if user is defining a url or inner html
@@ -26,9 +63,15 @@ angular.module('bossy.dropdown', [])
 			},
 			controller: function($scope) {
 				var thisDropdown = this;
+				/** 
+				* @param title 	member data that user wants dropdown ordered by
+				*/
 				thisDropdown.title = $scope.config.title;
 				thisDropdown.items = [];
 
+				/**
+				*@param src		location of json data that will populate dropdown; either local or remote
+				*/
 				//Retrieve json containing objects to populate the dropdown.
 				if($scope.config.src) {
 					//Checks that config.src is a JSON file.
